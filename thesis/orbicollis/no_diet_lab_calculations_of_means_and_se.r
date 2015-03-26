b7aac2c7b0f0cd52ce093ec322b1baf2b7bfc163
Robert Stanton <rstanton@illinois.edu>
2015-03-26 17:56:44 -0500

    Added several more analyses

--- thesis/orbicollis/no_diet_lab_calculations_of_means_and_se.r
+++ thesis/orbicollis/no_diet_lab_calculations_of_means_and_se.r
@@ -73,13 +73,40 @@ lsmeans(residpremass.rg, "Treatment")
 #conduct pairwise comparisons between treatments using the scheffe test
 pairw.anova(y=diet_lab_data$resid_premass_calculated, x=diet_lab_data$Treatment, method="scheffe")
 
+#post mass
+model <- lm(ln_postmass ~ Sex + Treatment + Measured + Sex:Treatment + Sex:Measured + Measured:Treatment + Sex:Treatment:Measured, data = diet_lab_data, na.action=na.omit)
+postmass.anova <- Anova(model, type=c(3))
+postmass.rg <- ref.grid(model)
+lsmeans(postmass.rg, "Sex")
+lsmeans(postmass.rg, "Measured")
+lsmeans(postmass.rg, "Treatment")
+#conduct pairwise comparisons between treatments using the scheffe test
+pairw.anova(y=diet_lab_data$ln_postmass, x=diet_lab_data$Treatment, method="scheffe")
+
+#post condition calculated
+model <- lm(resid_postmass_calculated ~ Sex + Treatment + Measured + Sex:Treatment + Sex:Measured + Measured:Treatment + Sex:Treatment:Measured, data = diet_lab_data, na.action=na.omit)
+residpostmass.anova <- Anova(model, type=c(3))
+residpostmass.rg <- ref.grid(model)
+lsmeans(residpostmass.rg, "Sex")
+lsmeans(residpostmass.rg, "Measured")
+lsmeans(residpostmass.rg, "Treatment")
+#conduct pairwise comparisons between treatments using the scheffe test
+pairw.anova(y=diet_lab_data$resid_postmass_calculated, x=diet_lab_data$Treatment, method="scheffe")
+
 #test for effect of starvation treatment
 df <- subset(diet_lab_data, select = c("BeetleID", "Sex", "Treatment", "Measured", "ln_premass_mg", "ln_postmass_mg"))
-
 dfL <- reshape(df, varying = list(5:6), idvar = "BeetleID", direction = "long", v.names = "ln_mass_mg")
 dfL$time <- factor(dfL$time, levels = 1:2, labels = c("pre", "post"))
 head(dfL); tail(dfL)
 aovMod <- aov(ln_mass_mg ~ Sex * Treatment * Measured * time + Error(BeetleID/time), data = dfL)
+
+#repeated measures ANOVA for condition calculated
+df <- subset(diet_lab_data, select = c("BeetleID", "Sex", "Treatment", "Measured", "resid_premass_calculated", "resid_postmass_calculated"))
+dfL <- reshape(df, varying = list(5:6), idvar = "BeetleID", direction = "long", v.names = "resid_mass_calc")
+dfL$time <- factor(dfL$time, levels = 1:2, labels = c("pre", "post"))
+head(dfL); tail(dfL)
+aovMod <- aov(resid_mass_calc ~ Sex * Treatment * Measured * time + Error(BeetleID/time), data = dfL)
+
 
 #model <- lm(cbind(ln_premass_mg, ln_postmass_mg) ~ Sex + Treatment + Measured + Sex:Treatment + Sex:Measured + Measured:Treatment + Sex:Treatment:Measured, data = diet_lab_data, na.action=na.omit)
 #effect_of_treatment <- Anova(model, type=c(3))
